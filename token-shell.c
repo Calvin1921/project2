09a87327debfa3337dd62902bd1376f597a563d3
jziesing <jziesing@gmail.com>
2013-05-26 17:12:32 -0700

    Jacks edit, hopefully it works

--- token-shell.c
+++ token-shell.c
@@ -11,168 +11,241 @@
 
 
 /**
- * Main program execution
- */
-#define maxArgLength	256
-char *argl[maxArgLength]; 
+* Main program execution
+*/
+#define maxArgLength 256
+char *argl[maxArgLength];
 pid_t pid = 0;
+int pipecount = 0;
+int leftcount = 0;
+int rightcount = 0;
 int tokencount = 0;
-int pipe_len = 0;
 
-
-void redirect(TOKENIZER *tokenizer){
-printf( "		Enter redirect method \n" );
-	int i = 0;
-	for( i=0; i<tokencount; i++ ) {
-		printf(" ------------------------------------ i = %d\n", i);
-		int in, out;
-		char *temp = get_next_token( tokenizer );
-				
-		if(*temp == '>') {
-			char *output_file = get_next_token( tokenizer );
-			printf( "(>) go to check 1 \n" );
-			printf(" 		output_file %s\n", output_file);
-			out = open( output_file, O_CREAT | O_RDWR, 0644);
-			dup2(out, STDOUT_FILENO);
-			i++;
-				
-		} else if(*temp == '<') {
-			char *input_file = get_next_token( tokenizer );
-			printf( "(<) go to check 2  \n" );
-			printf(" 		input_file %s\n", input_file);
-			in = open( input_file, O_RDWR, 0644);
-			dup2(in, STDIN_FILENO);
-			i++;
-		} if(*temp == '|'){
-			return ;
-		}else {
-			argl[i] = temp;
-		}
-	}
-	argl[tokencount] = NULL;
-	free_tokenizer( tokenizer );
-	printf( "Enter exit \n" );
-
-}
-
-void runpipe(int fd[], int cpid, char *path){
-	if(cpid == 0){
-		printf( "--------child--------\n" );
-	 /* child */
-	 	printf( "	%d\n",fd[0]);
-		dup2(fd[0], STDOUT_FILENO);
-		close(fd[1]);	/* the child does not need this end of the pipe */
-		printf( "	%d\n",fd[0]);
-		printf( "	what is path: %s \n", path );
-		execvp(path, argl);
-		perror(path);
-	}else if(!cpid){
-	printf( "--------child--------" );
-		printf( "	%d\n",fd[1]);
-		dup2(fd[1], STDIN_FILENO);
-		close(fd[0]);	/* the parent does not need this end of the pipe */
-		printf( "	%d\n",fd[1]);
-		printf( "	what is path: %s \n", path );
-		execvp(path, argl);
-		perror(path);
-	}else{
-		perror("fork");
-		exit(1);
-	}
-}
-
-
-
-
-
-
-int main( int argc, char *argv[] ){
-  TOKENIZER *tokenizer;
-  char string[256] = "";
-  char *tok;
-  int br;
-  int pipefd[2];
-
-  string[255] = '\0';	  
-  printf( "\n\nSeaShell: \n" );
-//----------------------------------------------------------------
-  while ((br = read( STDIN_FILENO, string, 255 )) > 0) {
-    if (br <= 1)
-      continue;
-//-----------------------------------------------------------------
-    string[br-1] = '\0';   
-    tokenizer = init_tokenizer( string );
-    tokencount = 0;
-	pipe_len = 0;
-//-----------------------------------------------------------------    
-    while( (tok = get_next_token( tokenizer )) != NULL ) {
-    	printf( "token:%s\n",tok);
-    	if( *tok == '|'){
-    		pipe_len +=1;
-      	}
-      	tokencount += 1;
-      	free( tok );
-    } 
-printf( "How many token(tokencount): %d\n",tokencount);
-printf( "How many pipe(pipe_len): %d\n",pipe_len);
-    free_tokenizer( tokenizer );
-    tokenizer = init_tokenizer( string );
-//-----------------------------------------------------------------
-    pid = fork();
-	if(pid == 0 ) {
-		printf( "Enter pid ==0 \n" );
-		if( pipe_len ==0){
-		printf( "	Enter pipe_len ==0 \n" );
-			redirect(tokenizer);
-			printf( "	End for loop \n" );
-		}else if(pipe_len >0){
-			printf( "	Enter pipe > 0 \n" );
-			printf("	pipe");
-			
-			int i = 0;
-			for( i=0; i<tokencount; i++ ) {
-				printf(" ------------------------------------ i = %d\n", i);
-				int out;
-				char *temp = get_next_token( tokenizer );
-				
-				if(*temp == '>') {
-					char *output_file = get_next_token( tokenizer );
-					printf( "(>) go to check 1 \n" );
-					printf(" 		output_file %s\n", output_file);
-					out = open( output_file, O_CREAT | O_RDWR, 0644);
-					dup2(out, STDOUT_FILENO);
-					i++;				
-				}else if(*temp == '|'){
-					runpipe(pipefd, pid, get_next_token( tokenizer ));
-				}else {
-					argl[i] = temp;
-				}
-			}
-			argl[tokencount] = NULL;
-			free_tokenizer( tokenizer );
-			printf( "	what is argl[0]: %s \n", argl[0] );
-			printf( "	what is argl %s \n", *argl );
-			printf( "	what is argl[0]: %s \n", argl[1] );
-			printf( "	what is argl %s \n", *argl );
-			execvp(argl[0], argl);
-			perror("execvp error");
-			
-			_exit(2);
-		}
-		printf( "	what is argl[0]: %s \n", argl[0] );
+void noPipe() {
+	
+	if( leftcount == 0 && rightcount == 0 ) {	
+		argl[tokencount] = NULL;
 		execvp(argl[0], argl);
 		perror("execvp error");
-		printf( "	exit \n" );
 		_exit(2);
-	}else if(!pid) {
-		perror("pid fork error");	
 	} else {
-		int l;
-		wait(&l);	
+		int n = 0;
+		int check = 0;
+		for( n=0; n<tokencount; n++ ) {
+			int in, out;
+			//out
+			if(check == 1) {
+				out = open( argl[n], O_CREAT | O_WRONLY, 0644);
+				if(out == -1) {
+					perror("check1 open");
+				}
+				dup2(out, STDOUT_FILENO);
+				check = 0;
+			} else if( check == 2) {
+				in = open( argl[n], O_RDWR, 0644 );
+				if(in == -1) {
+					perror("check2 open");
+				}
+				dup2(in, STDIN_FILENO);
+				check = 0;
+			}
+			if(*argl[n] == '>') {
+				printf( "HEll Yea: \n" );
+				argl[n] = NULL;
+				check = 1;
+			} else if(*argl[n] == '<') {
+			//printf( "another HEll Yea: \n" );
+				argl[n] = NULL;
+				check = 2;
+			} 
+		}
+		argl[tokencount] = NULL;
+		printf( "%c",*argl[0] );
+		execvp(argl[0], argl);
+		perror("execvp error");
+		_exit(2);
 	}
-	printf( "SeaShell: \n" );
-  }
-  
-  printf( "\nBye!\n" );
-  return 0;			/* all's well that end's well */
+//end of method	works great
+}
+
+
+//-------this works for a single pipe or a pipe with a redirect for input 
+//-------like < symbol before the | and no redirects after |
+void onePipe() {
+	pid_t pid2 = 0;
+	int fd[2];
+	int n = 0;
+	int check = 0;
+	char *argP[tokencount];
+	char *argC[tokencount];
+	int tempSize = 0;
+	for(n=0; n<tokencount; n++ ) {
+		if(check == 1) {
+			if(*argl[n] =='<' || *argl[n] == '>') {
+				argC[tempSize] = NULL;
+			} else {
+				argC[tempSize] = argl[n];
+			}
+			tempSize++;
+		} else {
+			argP[n] = argl[n];
+			if(*argl[n] == '|') {
+				argP[n] = '\0';
+				check = 1;
+			}
+			if(*argl[n] == '<' || *argl[n] == '>') {
+				argP[n] = NULL;
+			}
+		}
+	}
+	argC[tempSize] = '\0';
+	/*
+	for(n=0; n<tokencount; n++) {
+		printf("argP: %s\n", argP[n]);
+		
+	}
+	for(n=0; n<tokencount; n++) {
+		printf("argC: %s\n", argC[n]);
+	}*/
+	
+	check = 0;
+	for( n=0; n<tokencount; n++ ) {
+		int in, out;
+		//out
+		if(check == 1) {
+			out = open( argl[n], O_CREAT | O_WRONLY, 0644);
+			if(out == -1) {
+				perror("check1 open");
+			}
+			dup2(out, STDOUT_FILENO);
+			check = 0;
+		} else if( check == 2) {
+			in = open( argl[n], O_RDWR, 0644 );
+			if(in == -1) {
+				perror("check2 open");
+			}
+			dup2(in, STDIN_FILENO);
+			check = 0;
+		} else if( check == 3 ) {
+			/*attempt to catch redirect symbols after pipe in input chars
+			int nc = 0;
+			int checkTwo = 0;
+			for(nc=0; nc<tokencount; nc++) {
+				int out = 0;
+				if(checkTwo == 1) {
+					out = open( argC[n], O_CREAT | O_WRONLY, 0644);
+					if(out == -1) {
+						perror("check1 open");
+					}
+					dup2(out, STDOUT_FILENO);
+				}
+				if(*argC[nc] == '>') {
+					checkTwo = 1;
+				}
+			}*/
+			printf("hello \n");
+			if(pipe(fd) == -1) {
+				perror("pipe error");
+			} else {
+				pid2 = fork();
+				if(pid2 == -1) {
+					perror("pipe1fork error");
+				} else if(pid2 == 0) {
+					dup2(fd[0], 0);
+					close(fd[1]);  
+					execvp(argC[0], argC);
+					_exit(2);
+				} else {
+					dup2(fd[1], 1);
+					close(fd[1]);
+					//printf("made it here\n");
+					execvp(argP[0], argP);
+					_exit(2);
+				}	
+			}
+		}
+		if(*argl[n] == '>') {
+			printf( "HEll Yea: \n" );
+			argl[n] = NULL;
+			check = 1;
+		} else if(*argl[n] == '<') {
+		//printf( "another HEll Yea: \n" );
+			argl[n] = NULL;
+			check = 2;
+		} else if(*argl[n] == '|') {
+			argl[n] = NULL;
+			check = 3;
+		} 
+	}
+	/*
+	argl[tokencount] = NULL;
+	printf( "%c",*argl[0] );
+	execvp(argl[0], argl);
+	perror("execvp error");
+	_exit(2);*/	
+}
+
+int main( int argc, char *argv[] )
+{
+	int l;
+	TOKENIZER *tokenizer;
+	char string[256] = "";
+	char *tok;
+	int br;
+	string[255] = '\0';  
+	printf( "\n\nSeaShell: \n" );
+	while ((br = read( STDIN_FILENO, string, 255 )) > 0) { 
+		if(br <= 1)
+			continue;
+		string[br-1] = '\0';  
+		tokenizer = init_tokenizer( string );
+		tokencount = 0;
+		pipecount = 0;
+		leftcount = 0;
+		rightcount = 0;
+		int i = 0;
+		while( (tok = get_next_token( tokenizer )) != NULL ) {
+			//char *tempora = tok;
+			tokencount += 1;
+			if( tok[0] == '<' ) {
+				leftcount += 1;
+			} else if( tok[0] == '>' ) {
+				rightcount += 1;
+			} else if( tok[0] == '|' ) {
+				pipecount += 1;
+			} 
+			argl[i] = tok;
+			i = i + 1;	
+			//free( tok );
+			//free( tempora );
+		}
+		
+		free_tokenizer( tokenizer );
+		pid = fork();
+		if(pid ==-1) {
+			perror("first fork error");
+		}
+		if(!pid) {
+			//---------------block 1----------no pipes----------------
+			if( pipecount == 0 ) {
+				noPipe();
+			}
+			//---------------block 2----------one pipe----------------
+			else if( pipecount == 1 ) {
+				onePipe();
+				
+			}
+			//---------------block 3----------two pipes----or many----- 
+			else {
+				//multiple pipes
+				
+			}
+		} else {
+			wait(&l);
+		}
+		printf( "SeaShell: \n" );		
+	 }
+
+	 printf( "\nBye!\n" );
+	 return 0; /* all's well that end's well */
 }
